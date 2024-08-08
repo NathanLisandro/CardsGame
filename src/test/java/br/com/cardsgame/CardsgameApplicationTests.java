3e95e24341847e8d283b5f1936e25180eda9cef1
unknown <nathanlisandro@outlook.com>
2024-08-08 19:55:37 -0300

    feat: inicio do projeto em java spring

---
+++ src/test/java/br/com/cardsgame/CardsgameApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cardsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
